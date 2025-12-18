72cfed79e32215705e22cf4f0d59d8e7f500cfd5
Moncif <m_fokroun@estin.dz>
2025-12-18 12:54:17 +0100

    fixed the values on the navBar from pixels to rem

--- src/Components/layout/navBar/NavBar.tsx
+++ src/Components/layout/navBar/NavBar.tsx
@@ -4,13 +4,13 @@ import logo from "@/assets/logo.svg";
 export const NavBar = () => {
   const [hovered, setHovered] = useState<boolean>(false);
 
-  const LogoTextClasses = "font-[800] text-[24px]";
-  const BaseTextClasses = "font-[700] text-[20px] cursor-pointer";
+  const LogoTextClasses = "font-[800] text-[1.5rem]";
+  const BaseTextClasses = "font-[700] text-[1.25rem] cursor-pointer";
 
   return (
-    <nav className="flex items-center justify-between bg-[#FBF6F699] bg-opacity-60 rounded-b-[30px] h-[110px] ">
-      <div className="flex items-center gap-[19px] pl-[30px] pt-[9.44px] pb-[24.86px]">
-        <img src={logo} alt="Logo" className="w-[59.67px] h-[75.7px]" />
+    <nav className="flex items-center justify-between bg-[#FBF6F699] bg-opacity-60 rounded-b-[30px] h-[6.88rem] pr-[6.8125rem] pl-[1.875rem] ">
+      <div className="flex items-center gap-[1.25rem]  pt-[0.59rem] pb-[1.55rem]">
+        <img src={logo} alt="Logo" className="w-[3.75rem] h-[4.75rem]" />
         <div className="pt-4">
           <span className={`${LogoTextClasses} `}>DZ </span>
           <span className={`${LogoTextClasses} text-[#DCA934]`}>Stagiaire</span>
@@ -36,7 +36,7 @@ export const NavBar = () => {
         </span>
       </div>
 
-      <div className="flex gap-[20px] pr-[109px]">
+      <div className="flex gap-[1.25rem] ">
         <Button variant="mo9a3ar" size="small" disabled={false}>
           Log in
         </Button>
